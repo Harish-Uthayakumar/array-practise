82f6ea23c0bc082928e66c1483e629b78d0cf0d1
curious harish <36083887+Harish-Uthayakumar@users.noreply.github.com>
2019-04-05 17:15:49 +0530

    Add files via upload

--- insertion_sorting.c
+++ insertion_sorting.c
@@ -1,44 +1,107 @@
 #include<stdio.h>
 
-void inserstion(int arr[], int n)
+int main()
 {
-	int key,j,i;
 
-	for(i = 1; i < n; i++)
+	printf("Enter size");
+	int n;
+	scanf("%d",&n);
+	int arr[n][n];
+	int num;int row;int column;int index;
+
+
+	for(int h = 0; h < n; h++)
 	{
 
-		key = arr[i];
-		j = i - 1;
-
-		while( j > 0 && arr[j] > key)
-
+		for(int k =0; k < n; k++)
 		{
 
-			arr[j+1] = arr[j];
-			j = j - 1;
+			arr[h][k] = 0;
+		}
+	}
+
+	for(int i = 0; i < n*n; i++)
+	{
+
+		scanf("%d",&num);
+		row = num%n;
+		index = 1;
+		for(int k =0; k < n & index == 1; k++)
+		{
+
+			if(arr[row][k] == 0)
+			{
+				arr[row][k] = num;
+				index = 0;
+
+			}
 		}
 
-		arr[j + 1 ] = key;
+
 	}
 
 
-	for(int j = 0; j < n; j++)
+
+	int arr1[4][4] = {{32,56,100,0},{45,29,89,21},{34,46,78,0},{27,99,67,23}};
+
+
+	int sum1[4] = {0,0,0,0};
+	for(int i = 0; i < 4 ; i++)
 	{
-		 printf("%d \n", arr[j]);
+		for( int j =0; j < 4; j++)
+		{
+			sum1[i] = sum1[i] + arr1[i][j];
+
+		}
+	}
+int sum[n];
+
+		for(int h =0; h < n; h++)
+			{
+				sum[h] = 0;
+			}
+	for(int i = 0; i < n; i++)
+	{
+
+		for(int k = 0; k< n;k ++)
+		{
+			sum[i] = sum[i] + arr[i][k];
+
+		}
+	}
+
+	int count = 0;
+
+	for(int y = 0; y < n; y++)
+	{
+		if(sum[y] == sum1[y])
+			count++;
+	}
+
+	if(count == n)
+	{
+		printf("This is a combination");
+	}
+	else 
+	{
+		printf("This is not a combination" );
 	}
 
 
 
-}
 
+	for(int j =0; j < n; j++)
+	{
 
-int main()
+		for(int k = 0; k < n; k++)
+		{
 
-{
+			printf("%d \t",arr[j][k]);
 
-	int arr[] = {1,7,55,33};
-	int n = sizeof(arr)/sizeof(arr[0]);
-	inserstion(arr,n);
+		}
+
+		printf("\n");
+	}
 
 
 }
